46e08254b2e7bfa7eb4fe6d7c97b75c8f04ae59e
vanifufu <111576374+vani0-0@users.noreply.github.com>
2024-10-17 19:40:39 +0800

    fix: tailwind config, and removed react-helmet-async

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,15 +1,12 @@
 import Layout from '@/layout'
-import Seo from '@/seo'
-import { HelmetProvider } from 'react-helmet-async'
 
 function App() {
   return (
-    <HelmetProvider>
-      <Seo />
-      <Layout>
-        <div>Test Deploy</div>
-      </Layout>
-    </HelmetProvider>
+    <Layout>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </Layout>
   )
 }
 export default App
